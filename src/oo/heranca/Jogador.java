ba07c015712aaee69c31b1480f382d376e5a3906
Jackson <limajackson115@gmail.com>
2026-08-04 20:31:16 -0300

    Teste de Commit, segundo commit com exercicios das aulas de herança.

--- src/oo/heranca/Jogador.java
+++ src/oo/heranca/Jogador.java
@@ -2,8 +2,24 @@ package oo.heranca;
 
 public class Jogador {
 
+	int vida = 100;
 	int x;
 	int y;
+	
+	boolean atacar(Jogador oponente) { // boolean para saber se o ataque ocorreu ou não
+		int deltaX = Math.abs(x - oponente.x); // Para saber o valor absoluto,
+		int deltaY = Math.abs(y - oponente.y); // e se der -1 o resultado fica 1
+		
+		if(deltaX == 0 && deltaY == 1) { // Como se os jogadores estivessem um do lado do outro
+			oponente.vida -= 10;
+			return true;
+		} else if(deltaX == 1 && deltaY == 0) {
+			oponente.vida -= 10;
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	boolean andar (Direcao direcao) { // Método indicando os lados que o jogador pode andar
 		switch(direcao) {
