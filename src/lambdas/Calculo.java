005feb4ab8ff9cf77158c6735dd3166ee460fd4f
Jackson <limajackson115@gmail.com>
2026-08-18 20:16:02 -0300

    Primeira Função Lambda / Usando uma Interface com um único método

---
+++ src/lambdas/Calculo.java
@@ -0,0 +1,11 @@
+package lambdas;
+
+public interface Calculo {
+
+	public abstract double executar(double a, double b); 
+	/**
+	 *  Não é obrigatório deixar o "public" e o "abstract" já que são
+	 *  introduzidos de forma implicita
+	 */
+	
+}
---
+++ src/lambdas/CalculoTeste2.java
@@ -0,0 +1,19 @@
+package lambdas;
+
+public class CalculoTeste2 {
+
+	public static void main(String[] args) {
+		
+		// Pode-se escrever em uma linha só pq tem pouca coisa
+		Calculo calc = (x, y) -> { return x + y; };
+		System.out.println(calc.executar(2, 3));
+		
+		calc = (x, y) -> x * y;
+		/**
+		 *  Já que na segunda parte está sem os parenteses, haverá 
+		 * de forma implicita um "return" que nem na situação de cima;
+		 */
+		System.out.println(calc.executar(2,  3));
+		
+	}
+}
