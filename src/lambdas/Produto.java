b4ad95b47e18918a5d87d3d1c7c7e6b954e31421
Jackson <limajackson115@gmail.com>
2026-08-24 21:39:05 -0300

    Consumer

--- src/lambdas/Produto.java
+++ src/lambdas/Produto.java
@@ -13,5 +13,8 @@ public class Produto extends Object{
 		this.desconto = desconto;
 	}
 	
-	
+	public String toString() {
+		double precoFinal = preco * (1 - desconto);
+		return nome + " Tem preço: R$" + precoFinal;
+	}
 }
