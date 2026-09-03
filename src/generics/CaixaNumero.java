e2fdf59905d64bf1e30ca5510d13ac32714f2320
Jackson <limajackson115@gmail.com>
2026-09-03 17:13:12 -0300

    Generics e Herança #02

---
+++ src/generics/CaixaNumero.java
@@ -0,0 +1,7 @@
+package generics;
+
+// Number.lang é a classe Pai do Integer, Double, Float
+public class CaixaNumero<N extends Number> extends Caixa<N> {
+	// Essa é uma forma de fazer com que o "N" é obrigatóriamente da classe Number
+
+}
