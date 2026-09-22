c4881787c4f9e4c8bca0f11b6bca847078432124
Jackson <limajackson115@gmail.com>
2026-09-21 21:38:01 -0300

    Externalizando Dados de Conexão com o Properties

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -1,20 +1,32 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
 	public static Connection getConexao() {
 		try {
-			final String url = "jdbc:mysql://localhost/curso_java";
-			final String usuario = "root";
-			final String senha = "reservado";
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
 			
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String caminho = "/conexao.properties"; // Aqui é o file criado com os dados do banco de dados
+		// O ideal é criar uma pasta fora do src com esse arquivo, além de não subir ele pro github
+		prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+		return prop;
+		
 	}
 }
